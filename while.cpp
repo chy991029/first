043da633cd03c37b5fb8b3ab21ad077f6d1b94e1
chy991029 <104288585+chy991029@users.noreply.github.com>
2022-04-28 17:07:43 +0800

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+//int main()
+//{
+	//int ch = 0;
+	//EOF---文件结束标志
+	//*while ((ch = getchar()) != EOF)
+	//{
+	//	putchar(ch);
+	//}*/
+
+
+
+	//int ret = 0;
+	//int ch =0;
+	//char password[20] = { 0 };
+	//printf("请输入密码：>");
+	//scanf("%s", password);//输入密码，并存放在password数组中
+	//while ((ch = getchar()) != '\n')//把缓冲区剩余的‘\n'读完
+	//{
+	//	;
+	//}
+	//printf("请确认（Y/N）：>");
+	//ret = getchar();
+	//if (ret == 'Y')
+	//{
+	//	printf("确认成功\n");
+	//}
+	//else
+	//{
+	//	printf("放弃确认\n");
+	//	    
+	//}
+
+
+
+	/*int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch<'0' || ch>'9')
+			continue;
+		putchar(ch);
+	}*///只输出数字字符    
+	//return 0;
+//}
+
+
+
+
