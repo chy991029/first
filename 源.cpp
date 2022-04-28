9f794ecbcf034fa48b30267f942e3060941fc5a7
chy991029 <104288585+chy991029@users.noreply.github.com>
2022-04-28 16:10:07 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int age = 400;
+//		if (age < 18)
+//			printf("为成年\n");
+//		else if (age>18, age < 28)
+//			printf("2成年\n");
+//
+//	return 0;
+//}
+///
+//int main()
+//{
+//	int num = 4;
+//	if (num)
+//	{
+//		printf ("呵呵\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		if (i % 2 ==1)
+//			printf("%d", i);
+//		i ++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	scanf("%d", &a);
+//	switch (a)
+//	{
+//	case 1:
+//			printf("星期一\n");
+//	case 2:
+//		printf("星期二\n");
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//	}
+//
+//	return 0;
+//
+//}
+0
