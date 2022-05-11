ee6946c2568157f7c7d72e96b6a37803d217b739
chy991029 <104288585+chy991029@users.noreply.github.com>
2022-05-11 22:38:20 +0800

    Add files via upload

---
+++ 循环.cpp
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+///*int main()*///输入三个数按从大到小排
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int tmp = 0;
+//	scanf("%d%d%d", &a,  &b,  &c);
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+
+
+//int main()三的倍数
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//	return 0;
+//}
+
+
+
+
+//int main()最大公约数
+//{
+//	int m = 0;
+//	int n = 0;
+//	int r = 0;
+//	scanf("%d%d", &m, &n);
+//	while (r=m%n)
+//	{
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1000; i <= 2000; i++)
+////	{
+////		if (i % 4 == 0 && i % 100 != 0)     
+//
+//
+//if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0))
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+////		else if (i % 400 == 0)
+////		{
+////			printf("%d ", i);
+////			count++;
+////		}
+////	}
+//	printf("\ncount =%d\n", count);
+//	return 0;
+//}
+
