082d9d065918bd6d1695bcc2cc1de715416f3fba
chy991029 <104288585+chy991029@users.noreply.github.com>
2022-05-12 21:29:17 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,56 +1,77 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 //int main()
 //{
-//	int age = 400;
-//		if (age < 18)
-//			printf("为成年\n");
-//		else if (age>18, age < 28)
-//			printf("2成年\n");
-//
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//	        //判断i是否为素数
+//			//1.试除法
+//			//产生2-i-1
+//		int j = 0;
+//		for (j = 2; j < i; j ++)
+//		{
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j == i)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//		
 //	return 0;
 //}
-///
+#include<math.h>
 //int main()
 //{
-//	int num = 4;
-//	if (num)
+//	int i = 0;
+//	int j = 0;
+//	for (i = 100; i <= 200; i++)
 //	{
-//		printf ("呵呵\n");
+//		
+//		for (j = 1; j <= sqrtf(i);j++)
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
 //	}
+//	if (j >sqrtf(i))
+//	{
+//		printf("%d ", i);
+//	}
+//	
 //	return 0;
 //}
+
+
 //int main()
 //{
-//	int i = 1;
-//	while (i <= 100)
+//	int  i = 0;
+//	int count = 0;
+//	for (i = 101; i <= 200; i += 2)
 //	{
-//		if (i % 2 ==1)
-//			printf("%d", i);
-//		i ++;
+//		int j = 0;
+//		for (j = 2; j <= sqrt((float) i); j++)
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		if (j > sqrt((float)i))
+//		{
+//			 count++;
+//			 printf("%d ", i);
+//		}
 //	}
+//	printf("\ncount=%d\n", count);
 //	return 0;
 //}
-//int main()
-//{
-//	int a = 1;
-//	scanf("%d", &a);
-//	switch (a)
-//	{
-//	case 1:
-//			printf("星期一\n");
-//	case 2:
-//		printf("星期二\n");
-//	case 3:
-//		printf("星期三\n");
-//		break;
-//	case 4:
-//		printf("星期四\n");
-//	}
-//
-//	return 0;
-//
-//}
-0
+
+
+
+
